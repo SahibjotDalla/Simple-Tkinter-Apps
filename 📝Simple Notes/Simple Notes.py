a27716b21f8d0d2360fc0658169a5130dcdb7f37
Sahibjot Dalla <126424781+SahibjotDalla@users.noreply.github.com>
2024-08-19 19:12:08 -0400

    Update Simple Notes
    Added:
    - Option To Add New Notes
    - Option To Delete Notes
    - Homepage
    - Visual Improvements

--- 📝Simple Notes/Simple Notes.py
+++ 📝Simple Notes/Simple Notes.py
@@ -1,29 +1,60 @@
+import customtkinter
 from customtkinter import *
 
 window = CTk()
-window.geometry('400x550')
+window.geometry('380x520')
+window.minsize(380, 520)
 window.title('Notes')
 
-colourVar = 0
+Notes = 0
 
 set_default_color_theme('blue')
 
 def theme_change():
+
     if get_appearance_mode() == 'Dark':
         set_appearance_mode('light')
+
     else:
         set_appearance_mode('dark')
 
-CTkLabel(master = window, text = 'Simple Notes', font = ('Dubai', 24, 'bold'), text_color = '#1F6AA9').pack()
+def create_note():
+    global Notes
+
+    if Notes < 10:
+        Notes += 1
+        tabview.add(str(Notes))
+        CTkTextbox(master = tabview.tab(str(Notes)), font = ('Dubai', 12), width = 320, height = 420).pack()
+
+    return Notes
+
+def close_note():
+    global Notes
+    Notes -= 1
+
+    if tabview.get() == '🏠':
+        pass
+    else:
+        tabview.delete(tabview.get())
+
+    return Notes
 
 tabview = CTkTabview(master = window)
-tabview.add('Note 1')
-tabview.add('Note 2')
+tabview.add('🏠')
 tabview.pack()
 
-CTkTextbox(master = tabview.tab('Note 1'), font = ('Dubai', 12), width = 320, height = 420).pack()
-CTkTextbox(master = tabview.tab('Note 2'), font = ('Dubai', 12), width = 320, height = 420).pack()
+CTkLabel(master = tabview.tab('🏠'), text = 'Simple Notes', font = ('Dubai', 24, 'bold'), text_color = '#1F6AA9').pack(side = TOP)
+CTkLabel(master = tabview.tab('🏠'), text = 'Welcome To Simple Notes!', font = ('Dubai', 12)).pack(side = TOP)
+CTkLabel(master = tabview.tab('🏠'), text = 'Press 🎨 to change between light and dark mode.', font = ('Dubai', 12)).pack(side = TOP)
+CTkLabel(master = tabview.tab('🏠'), text = 'Press ➕ to create a new note.', font = ('Dubai', 12)).pack(side = TOP)
+CTkLabel(master = tabview.tab('🏠'), text = 'Press ❌ to delete a note.', font = ('Dubai', 12)).pack(side = TOP)
 
-CTkButton(master = window, text = 'Theme', font = ('Dubai', 12, 'bold'), width = 20, height = 20, command = theme_change).pack(side = 'top', padx = 3, pady = 6)
+Button_Frame = CTkFrame(master = window)
+
+CTkButton(master = Button_Frame, text = '🎨', font = ('Dubai', 10), width = 20, height = 20, command = theme_change).pack(side = 'left', padx = 3, pady = 3)
+CTkButton(master = Button_Frame, text = '➕', font = ('Dubai', 8), width = 22, height = 22, command = create_note).pack(side = 'left')
+CTkButton(master = Button_Frame, text = '❌', font = ('Dubai', 8), width = 22, height = 22, command = close_note).pack(side = 'left', padx = 3, pady = 3)
+
+Button_Frame.pack(pady = 5)
 
 window.mainloop()
